fed6248f29acba97370d22f9f0cfd5428819aad6
luisciaramicoli <luisgustavobonfim996@gmail.com>
2024-06-30 08:32:10 -0300

    atualizações no código da página principal

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './home.module.css';
+import styles from './home.module.css';
+import { FaBriefcase } from "react-icons/fa";
 
 function Home() {
   return (
-   <div className='fundo'>
-    <Link to = "/cadastro">Cadastro</Link>
-    <Link to = "/login">Login</Link>
-   </div>
+    <div className={styles.fundo}>
+      <header className={styles.cabecalho}>
+        <Link to="/cadastro"><div className={styles.link}>Cadastro</div></Link>
+        <Link to="/login"> <div className={styles.link}>Login</div></Link>
+      </header>
+
+      <div className={styles.page1}>
+        <div className={styles.titulo}>
+          SAT
+        </div>
+        <div className={styles.baixoTitulo}>
+          Site Administrativo Tecnológico
+        </div>
+      </div>
+
+      <div className={styles.topicos}>
+        <div className={styles.topico1}>
+          <div className={styles.circulo}>
+            <i className={styles.icone}><FaBriefcase /></i>
+          </div>
+
+        </div>
+
+
+        <div className={styles.topico2}>
+          <div className={styles.circulo}>
+            <i className={styles.icone}><FaBriefcase /></i>
+          </div>
+
+        </div>
+
+        <div className={styles.topico3}>
+          <div className={styles.circulo}>
+            <i className={styles.icone}><FaBriefcase /></i>
+          </div>
+
+        </div>
+      </div>
+
+      <div className={styles.textos}>
+        <div className={styles.textoTopico1}>
+          Texto abaixo do terceiro cskkkkkkkkkkkkkkkkk
+        </div>
+        <div className={styles.textoTopico2}>
+          Texto abaixo do terceiro círculoscsdcsd
+        </div>
+        <div className={styles.textoTopico3}>
+          Texto abaixo do terceiro círculoscsdcsdsds
+        </div>
+      </div>
+
+    </div>
   );
 }
 
