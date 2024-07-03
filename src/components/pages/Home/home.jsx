20007cebfd84ec191ac2d003f08d15d03c6c2ee7
luisciaramicoli <luisgustavobonfim996@gmail.com>
2024-07-03 09:15:13 -0300

    cabecalho

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -51,9 +51,9 @@ function Home() {
 
         <div className={styles.textos}>
           <p>
-            O cadastro de funcionários no STA é projetado para ser rápido e eficiente, com uma interface intuitiva e campos claros que minimizam erros.
+            O cadastro de funcionários no SAT é projetado para ser rápido e eficiente, com uma interface intuitiva e campos claros que minimizam erros.
             Isso economiza tempo, melhora a precisão das informações e facilita a gestão e atualização de dados.
-            A tecnologia do STA visa simplificar processos administrativos e aumentar a eficiência do seu negócio.</p>
+            A tecnologia do SAT visa simplificar processos administrativos e aumentar a eficiência do seu negócio.</p>
         </div>
 
       </div>
@@ -65,15 +65,15 @@ function Home() {
             <img src="/cadastro funcionário.png" alt="" className={styles.cadastroFuncionarioImg} />
           </div>
           <div className={styles.textPage3}>
-            <p>O cadastro de funcionários no STA é projetado para ser simples e rápido, garantindo que novos membros da equipe sejam adicionados ao sistema sem demora. O processo simplificado de cadastro economiza tempo, permitindo que você se concentre em outras tarefas importantes. Além disso, a interface intuitiva e os campos claros ajudam a minimizar erros de entrada de dados, garantindo maior precisão nas informações registradas. Com um sistema de cadastro eficiente, a organização e a gestão das informações de clientes e funcionários são melhoradas, facilitando o acesso e a atualização de dados. No STA, acreditamos que a tecnologia deve facilitar a vida das pessoas, e nosso sistema de cadastro é um exemplo de como a inovação pode simplificar processos administrativos e aumentar a eficiência do seu negócio.</p>
+            <p>O cadastro de funcionários no SAT é projetado para ser simples e rápido, garantindo que novos membros da equipe sejam adicionados ao sistema sem demora. O processo simplificado de cadastro economiza tempo, permitindo que você se concentre em outras tarefas importantes. Além disso, a interface intuitiva e os campos claros ajudam a minimizar erros de entrada de dados, garantindo maior precisão nas informações registradas. Com um sistema de cadastro eficiente, a organização e a gestão das informações de clientes e funcionários são melhoradas, facilitando o acesso e a atualização de dados. No STA, acreditamos que a tecnologia deve facilitar a vida das pessoas, e nosso sistema de cadastro é um exemplo de como a inovação pode simplificar processos administrativos e aumentar a eficiência do seu negócio.</p>
           </div>
         </div>
       </div>
 
       <div className={styles.page4}>
-        <div className={styles.tituloPage4}>Objetivo e Missão do STA</div>
+        <div className={styles.tituloPage4}>Objetivo e Missão do SAT</div>
         <div className={styles.textoPage4}>
-          <p>O cadastro de funcionários no STA é rápido e fácil, com uma interface intuitiva que reduz erros e economiza tempo.
+          <p>O cadastro de funcionários no SAT é rápido e fácil, com uma interface intuitiva que reduz erros e economiza tempo.
             Esse sistema eficiente melhora a organização e gestão de dados, simplificando processos administrativos e aumentando a eficiência do seu negócio.</p>
         </div>
       </div>
@@ -81,7 +81,7 @@ function Home() {
       <div className={styles.page5}>
         <div className={styles.tituloPage5}>Pagina inicial</div>
         <div className={styles.paginaPage5}>
-          <div className={styles.textpage5}>A dashboard do Site Tecnológico Administrativo (STA) oferece uma visão clara e intuitiva das operações administrativas.
+          <div className={styles.textpage5}>A dashboard do Site Tecnológico Administrativo (SAT) oferece uma visão clara e intuitiva das operações administrativas.
             Com uma interface personalizável e recursos como gráficos dinâmicos e notificações instantâneas, permite que gestores monitorem métricas em tempo
             real, analisem desempenho e tomem decisões rápidas e precisas, otimizando a gestão de recursos tecnológicos.
           </div>
