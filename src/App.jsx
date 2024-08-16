c530224f2af0d4a34da5ef63907609b8539b3c02
luisciaramicoli <luisgustavobonfim996@gmail.com>
2024-08-15 21:25:04 -0300

    Contato

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/pages/login/login';
-import Cadastro from './components/pages/cadastro/cadastro';
+import Contato from './components/pages/contato/contato';
 import Home from './components/pages/Home/home'; // Supondo que você tenha uma página inicial
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
     <Router>
      
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/contato" element={<Contato />} />
         </Routes>
     </Router>
   );
